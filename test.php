ace147df4e1de5e7c92f024fa622f9e1425c8b36
Olga-Student <orazina.2906@mail.ru>
2021-03-09 11:12:03 +0300

    TEST1

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+class User extends Model
+{
+    public $full_name;
+    public $email;
+
+    public function getByLogin($login)
+    {
+        $sql = "SELECT * FROM {$this->tablename} WHERE login = {$login}";
+        return $this->db->queryOne($sql);
+    }
+    public function getTableName()
+    {
+        return 'users';
+    }
+}
