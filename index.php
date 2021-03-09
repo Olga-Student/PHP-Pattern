ec62353b66056cc0a39340b77e131d1afaab2626
Olga-Student <orazina.2906@mail.ru>
2021-03-09 10:47:53 +0300

    Домашняя работа №1

--- index.php
+++ index.php
@@ -1,26 +1,61 @@
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-            crossorigin="anonymous"></script>
-</head>
 <?php
 class Product {
-public $artikl;
-public $title;
-public $size;
-public $price;
-public $category;
-public $warantlylife;}
+    public $artikl;
+    public $title;
+    public $size;
+    public $price;
+    public $category;
+    public $warantlylife;
+
+    //function __construct($artikl, $title, $size, $price, $category, $warantlylife)
+    public function addProduct(){
+
+    }
+    public function creatProduct(){
+
+    }
+    public function delProduct(){
+
+    }
+
+}
+class Label extends Product{
+    public $strokecod;
+}
+
+
+class A {
+    public function foo() {
+        static $x = 0;
+        echo ++$x;
+    }
+}
+$a1 = new A();
+$a2 = new A();
+$a1->foo();// при вызове функции присваивается статическое значение переменной х,
+// которое сохраняется после окончания работы функции.
+// В эхо выводится ее значение увеличенное на единицу, т е 1.
+
+
+$a2->foo();// ответ 2 не важно сколь объектов метод существует в одном экземпляре
+//последующие вызовы дадут увелечение каждый раз на один
+$a1->foo();// ответ 3
+$a2->foo();// ответ 4
+
+class C {
+    public function foo() {
+        static $y = 0;
+        echo ++$y;
+    }
+}
+class B extends C {
+}
+$a1 = new C();
+$b1 = new B();
+$a1->foo();//1  при вызове функции присваивается статическое значение переменной х,
+// которое сохраняется после окончания работы функции.
+// В эхо выводится ее значение увеличенное на единицу, т е 1.
+$b1->foo();//1 при наследовании класса был создан новый метод, который начал выполнение с начала
+$a1->foo();//2 при следующем вызове произойдет увеличение на один 1+1
+$b1->foo();//2
